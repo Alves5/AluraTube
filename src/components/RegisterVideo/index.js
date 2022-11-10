8967ed3b3927fe019494ff7ac4e4be263bb5b7b0
Daniel Alves <drodriguesalves2@gmail.com>
2022-11-10 18:50:45 -0300

    Update criando hooks

---
+++ src/components/RegisterVideo/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { StyledRegisterVideo } from "./Styles";
+
+function useForm(propsDoForm){
+    const [values, setValues] = React.useState(propsDoForm.initialValues);
+    return {
+        values,
+        handleChange: (e) => {
+            const value = e.target.value;
+            const name = e.target.name;
+            setValues({
+              ...values,
+              [name]: value,
+            });
+            },
+        clearForm(){
+            setValues({})
+        }
+    };
+}
+
+export default function RegisterVideo() {
+const formCadastro = useForm({
+    initialValues: { titulo: "Frost Punk", url: "https://youtube"}
+});
+  const [formVisivel, setFormVisivel] = React.useState(false);
+  
+
+  return (
+    <StyledRegisterVideo>
+      <button className="add-video" onClick={() => setFormVisivel(true)}>
+        +
+      </button>
+      {formVisivel ? (
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            setFormVisivel(false);
+            formCadastro.clearForm();
+        }}>
+          <div>
+            <button type="button"
+              className="close-modal"
+              onClick={() => setFormVisivel(false)}>
+              X
+            </button>
+            <input
+              placeholder="Titulo do vídeo"
+              name="titulo"
+              value={formCadastro.values.titulo}
+              onChange={formCadastro.handleChange}
+            />
+            <input 
+                placeholder="URL" 
+                name="url"
+                value={formCadastro.values.url}
+                onChange={formCadastro.handleChange}
+            />
+            <button type="submit">Cadastrar</button>
+          </div>
+        </form>
+      ) : (
+        false
+      )}
+    </StyledRegisterVideo>
+  );
+}
